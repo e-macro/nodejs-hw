06041eb1566430f8aaa96dc1fa30fb7a70e23b1b
Volodymyr Buhlak <suedanny2@yahoo.com>
2025-10-15 20:47:17 +0300

    completed task 05-mail-and-img

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -124,3 +124,27 @@ export const requestResetEmail = async (req, res, next) => {
   res.status(200).json({ message: 'Password reset email sent successfully' }
 );
 };
+
+export const resetPassword = async (req, res, next) => {
+  const { token, password } = req.body;
+
+  let payload;
+  try {
+    payload = jwt.verify(token, process.env.JWT_SECRET);
+  } catch {
+    return next(createHttpError(401, 'Invalid or expired token'));
+  };
+
+  const user = await User.findOne( { _id: payload.sub, email: payload.email } );
+  if (!user) {
+    next(createHttpError(404, 'User not found'));
+    return;
+  };
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  await User.updateOne(
+    { _id: user._id },
+    { password: hashedPassword },
+  );
+  res.status(200).json({ message: 'Password reset successfully' });
+};
--- src/models/user.js
+++ src/models/user.js
@@ -4,6 +4,11 @@ const userSchema = new Schema({
   username: { type: String, required: false, trim: true },
   email: { type: String, required: true, unique: true, trim: true },
   password: { type: String, required: true },
+  avatar: {
+    type: String,
+    required: false,
+    default: "<https://ac.goit.global/fullstack/react/default-avatar.jpg>"
+  }
 }, {timestamps: true, versionKey: false});
 
 userSchema.pre('save', function(next) {
